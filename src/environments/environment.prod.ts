62e244c699df01028a92e1b13223a78378aa659d
hasan@igit.spb.ru <hasan@igit.spb.ru>
2023-02-20 01:45:08 +0300

    Auto-generated commit [ci skip]

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  mapbox: {
-    accessToken:
-      'pk.eyJ1IjoibWFrZW5vaXplciIsImEiOiJjazN1YWwzZmYwYXJ3M25xOG9jcjJ0bjZlIn0.ylHg0tCIXrvWRuB9ttVpOw',
-  },
-};
